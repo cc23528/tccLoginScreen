602c77c15b6da342e19a382f3fb5bddabc6c157d
Verissimo <cc23528@g.unicamp.com.br>
2024-05-16 22:50:57 -0300

    att
    att

--- LoginScreenTCC/app/src/main/java/com/example/loginscreentcc/ui/LoginFragment.kt
+++ LoginScreenTCC/app/src/main/java/com/example/loginscreentcc/ui/LoginFragment.kt
@@ -1,13 +1,17 @@
-package com.aldemir.cursoandroidkotlin.ui
+package com.example.loginscreentcc.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.aldemir.cursoandroidkotlin.NavigationHost
-import com.aldemir.cursoandroidkotlin.R
-import kotlinx.android.synthetic.main.fragment_login.*
+import android.widget.Button
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.example.loginscreentcc.NavigationHost
+
+
+import com.example.loginscreentcc.R
+//import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
@@ -30,12 +34,13 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //era pro login_button.setOnClickListener pegar do fragment_login.xml mas nao to conseguindo importar
-        //tem q implementar o navigationHost tbm
-        login_button.setOnClickListener {
+        val btnLogin = view.findViewById<Button>(R.id.login_button)
+        btnLogin.setOnClickListener {
             (activity as NavigationHost).navigateTo(HomeFragment(), false)
         }
     }
+
+
 
     companion object {
 
--- LoginScreenTCC/app/src/main/java/com/example/loginscreentcc/ui/TestFragment.kt
+++ LoginScreenTCC/app/src/main/java/com/example/loginscreentcc/ui/TestFragment.kt
@@ -1,14 +1,11 @@
 package com.example.loginscreentcc.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
 import com.example.loginscreentcc.R
-//import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [TestFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class TestFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,16 +34,10 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fra  gment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val toolbar = view.findViewById<Toolbar>(R.id.app_bar)
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-
-    }
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -54,12 +45,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment TestFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            TestFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
